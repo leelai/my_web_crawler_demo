1093f914595584ad0faeb05b1e7ac27b4eb916f9
Lai Li(李騋) <Lai_Li@HTC.COM>
2022-01-04 14:10:33 +0800

    add mac

--- checkTime.py
+++ checkTime.py
@@ -8,7 +8,7 @@ headers = {
 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
 }
 
-macs = ["e4:a4:71:04:6b:36", "a0:78:17:73:f4:61"]
+macs = ["e4:a4:71:04:6b:36", "a0:78:17:73:f4:61", "90:e8:68:16:ae:a9"]
 
 def canIUse():
     mac = gma()
